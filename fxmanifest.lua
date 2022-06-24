97b53db91d78a2294abf8a5a2507c9ef7ecedafb
PrivateGiles <56416493+PrivateGiles@users.noreply.github.com>
2022-06-24 21:41:56 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean' 
+ games { 'gta5' } 
+  
+ author 'PrivateGiles' 
+ description 'Disable those Pesky NPCs flying about in helicopters!' 
+ version '2.0.0' 
+  
+ client_script 'client.lua'
