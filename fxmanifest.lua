6b40dc9d0eeb57e12a87c5fe71ae4b467272aa0a
PrivateGiles <56416493+PrivateGiles@users.noreply.github.com>
2022-07-17 21:57:22 +0100

    Fixed an issue with the fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
  games { 'gta5' } 
   
  author 'PrivateGiles' 
- description 'Disable those Pesky NPCs flying about in helicopters!' 
+ description 'Disable those NPCs flying about in Helicopters and planes' 
  version '2.0.0' 
   
  client_script 'client.lua'
